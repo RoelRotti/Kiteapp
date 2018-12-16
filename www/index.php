97945cc19743c5c1747b4293282f14a1d08fcc37
RoelRotti <roel.rotteveel@gmail.com>
2018-12-16 13:00:44 +0100

    Update index.php

--- www/index.php
+++ www/index.php
@@ -26,6 +26,30 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'
 	exit;
 }
 
+
+function process_file($file, array &$errors = array())
+{
+	if ($file['error'] != 0)
+		return "Er is een fout opgetreden bij het uploaden.";
+
+	$reader = new KnowledgeBaseReader;
+	$errors = $reader->lint($file['tmp_name']);
+
+	$unique_name = sha1(microtime() . uniqid('kb', true)) . '.xml';
+
+	if (count($errors) > 0)
+		return false;
+
+	if (!move_uploaded_file($file['tmp_name'], '../knowledgebases/' . $unique_name))
+	{
+		$errors[] = "De knowledge-base kon niet worden opgeslagen op de server.";
+		return false;
+	}
+	
+	return $unique_name;
+}
+
+
 $template = new Template('templates/single.phtml');
 $template->errors = $errors;
 
